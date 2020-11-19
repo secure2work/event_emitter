e0c468eee992960cc4c66ca16886214703d3bf1e
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-19 18:24:17 +0500

    feat: example created

--- example/client.go
+++ example/client.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+
 	"github.com/bruteforce1414/event_emitter"
 )
 
@@ -8,4 +13,52 @@ func main() {
 	publisher1 := event_emitter.NewPubsub()
 	channel1, funcUnsubscribe1 := publisher1.Subscribe()
 	channel2, funcUnsubscribe2 := publisher1.Subscribe()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	var e1, e2 event_emitter.Event
+	go func() {
+		for {
+			wg.Add(1)
+			e1 = <-channel1
+			i, _ := strconv.Atoi(e1.Name)
+			if (i % 2) == 0 {
+				fmt.Println("1 channel log number", e1.Name)
+			}
+			fmt.Println(e1.Name)
+			wg.Done()
+		}
+	}()
+
+	go func() {
+		for {
+			wg.Add(1)
+			e2 = <-channel2
+			i, _ := strconv.Atoi(e2.Name)
+			if (i % 3) == 0 {
+				fmt.Println("2 channel log number", e2.Name)
+			}
+			wg.Done()
+
+		}
+	}()
+
+	go func() {
+		for {
+			event1 := event_emitter.Event{
+				Name:   fmt.Sprint(time.Now().Second()),
+				Params: make(map[string]interface{}),
+			}
+			publisher1.Emit(event1)
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
+	wg.Wait()
+	fmt.Println("e1 is", e1)
+	fmt.Println("e2 is", e2)
+
+	funcUnsubscribe1()
+	funcUnsubscribe2()
 }
