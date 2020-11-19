81fac68a43e66f7e0bb0c7e42faec27324197d29
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-19 13:27:46 +0500

    feat: Emit implemented

--- publisher.go
+++ publisher.go
@@ -7,30 +7,28 @@ import (
 
 type publisher struct {
 	counter uint64
-	mu   sync.RWMutex
-	subs map[uint64]chan<- Event
+	mu      sync.RWMutex
+	subs    map[uint64]chan<- Event
 }
 
-//unsubscribe func(p, nameChannel string)
-//Убрать содержимое скобок или нет?
 func (p *publisher) Subscribe() (evt <-chan Event, unsubscribe func()) {
 	newChannel := make(chan Event)
 	p.mu.Lock()
-	p.counter=p.counter+1
+	p.counter = p.counter + 1
 	nameChannel := p.counter
 	p.mu.Unlock()
 	p.subs[nameChannel] = newChannel
 
 	c := func() {
-		_, ok := p.subs[nameChannel];
+		_, ok := p.subs[nameChannel]
 		if ok {
 			close(newChannel)
 			p.mu.Lock()
-			delete(p.subs, nameChannel);
+			delete(p.subs, nameChannel)
 			p.mu.Unlock()
 			fmt.Println("nameChannel is", nameChannel)
 			fmt.Println("unsubscribe")
-		}else{
+		} else {
 			println("there's not channel with id ", nameChannel)
 		}
 	}
@@ -38,19 +36,25 @@ func (p *publisher) Subscribe() (evt <-chan Event, unsubscribe func()) {
 	return newChannel, c
 }
 
-func(p *publisher) Emit(event Event){
+func (p *publisher) Emit(event Event) {
 	p.mu.Lock()
+
 	for _, value := range p.subs {
-		value<-event
+
+		go func(evt chan<- Event) {
+			value <- event
+		}(value)
+		fmt.Println("channel is", value)
+		fmt.Println("event is", event)
 	}
 	p.mu.Unlock()
 }
-
 
 func NewPubsub() *publisher {
 	pub := &publisher{}
 	pub.mu.Lock()
 	pub.subs = make(map[uint64]chan<- Event)
+
 	pub.mu.Unlock()
 	return pub
 }
--- publisher_test.go
+++ publisher_test.go
@@ -6,13 +6,20 @@ import (
 )
 
 func TestPublisher_Subscribe(t *testing.T) {
-	pub:=NewPubsub()
-	ch1,funcUnsubscribe1:=pub.Subscribe()
-	ch2,funcUnsubscribe2:=pub.Subscribe()
+	pub := NewPubsub()
+	ch1, funcUnsubscribe1 := pub.Subscribe()
+	ch2, funcUnsubscribe2 := pub.Subscribe()
 
 	fmt.Println("channel", ch1)
 
 	fmt.Println("channel2", ch2)
+
+	eventTest := Event{
+		Name:   "all plugins inited",
+		Params: nil,
+	}
+
+	pub.Emit(eventTest)
 
 	//fmt.Println("func is", b)
 	funcUnsubscribe1()
