b7ff97152302acff0d64fef175c0b349e5faa7e3
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-26 20:30:29 +0500

    feat: tests for 2 subscribers with both have local and global middlewares

--- publisher_test.go
+++ publisher_test.go
@@ -244,3 +244,82 @@ func TestPublisher_On_2subscribers_2receiving_2localmiddleware(t *testing.T) {
 	funcUnsubscribe1()
 	funcUnsubscribe2()
 }
+
+func TestPublisher_On_2subscribers_2receiving_2_globalmiddleware_2localmiddleware(t *testing.T) {
+	pub := NewPubsub()
+
+	eventTest := Event{
+		Name:   "nori/plugins/started",
+		Params: make(map[string]interface{}),
+	}
+
+	mG1 := func(event *Event) {
+		event.Params["global_m1key"] = "global_m1value"
+	}
+
+	mG2 := func(event *Event) {
+		event.Params["global_m2key"] = "global_m2value"
+	}
+
+	mL1 := func(event *Event) {
+		event.Params["local_m1key"] = "local_m1value"
+	}
+
+	mL2 := func(event *Event) {
+		event.Params["local_m2key"] = "local_m2value"
+	}
+
+	pub.Use("nori/plugins/*", mG1, mG2)
+
+	ch1, funcUnsubscribe1 := pub.On("nori/plugins/*", mL1)
+	ch2, funcUnsubscribe2 := pub.On("nori/plugins/started", mL1, mL2)
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	var e1, e2 Event
+	go func() {
+		select {
+		case e1 = <-ch1:
+			wg.Done()
+		}
+	}()
+
+	go func() {
+		select {
+		case e2 = <-ch2:
+			wg.Done()
+		}
+	}()
+
+	pub.Emit(eventTest)
+
+	wg.Wait()
+
+	t.Log("params in 1st channel: ", e1.Params)
+	t.Log("params in 2nd channel: ", e2.Params)
+	assert.NotEqual(t, e1.Params, e2.Params)
+
+	eventForComparing1 := Event{
+		Name:   "nori/plugins/started",
+		Params: make(map[string]interface{}),
+	}
+	eventForComparing1.Params["global_m1key"] = "global_m1value"
+	eventForComparing1.Params["global_m2key"] = "global_m2value"
+	eventForComparing1.Params["local_m1key"] = "local_m1value"
+	assert.Equal(t, e1.Params, eventForComparing1.Params)
+
+	eventForComparing2 := Event{
+		Name:   "nori/plugins/started",
+		Params: make(map[string]interface{}),
+	}
+	eventForComparing2.Params["global_m1key"] = "global_m1value"
+	eventForComparing2.Params["global_m2key"] = "global_m2value"
+	eventForComparing2.Params["local_m1key"] = "local_m1value"
+	eventForComparing2.Params["local_m2key"] = "local_m2value"
+
+	assert.Equal(t, e2.Params, eventForComparing2.Params)
+
+	funcUnsubscribe1()
+	funcUnsubscribe2()
+}
