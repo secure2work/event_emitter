a8b408e111853f063bf9e4f061e07602b8a11fbb
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-30 11:11:21 +0500

    feat: benchmark test for Map function (deep copying) passes

--- utils/deepcopy_test.go
+++ utils/deepcopy_test.go
@@ -5,12 +5,13 @@ import (
 )
 
 func Benchmark_Map(b *testing.B) {
-	var copy map[string]interface{}
+	copy := make(map[string]interface{})
 
 	copy["global_m1key"] = "global_m1value"
 	copy["global_m2key"] = "global_m2value"
 	copy["local_m1key"] = "local_m1value"
 	copy["local_m2key"] = "local_m2value"
+
 	for i := 0; i < b.N; i++ {
 		Map(copy)
 	}
